2c227b31a0ea5e057dc98b859a2e2613b88a7645
Ilya Egorov <i.egorov@2gis.ru>
2014-12-19 09:59:16 +0600

    tommorow-today difference

--- exchange.php
+++ exchange.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 const USD = 9;
 const EUR = 10;
@@ -7,6 +7,7 @@ const SERVER = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=";
 
 $yesterday = date('d/m/Y', strtotime('-1 day'));
 $today = date('d/m/Y');
+$tommorow = date('d/m/Y', strtotime('+1 day'));
 
 function getValues($date) {
 
@@ -30,15 +31,22 @@ function getValues($date) {
 
 $data['yesterday'] = getValues($yesterday);
 $data['today']     = getValues($today);
+$data['tommorow']     = getValues($today);
+
 
 $data['diff']['usd'] = $data['today']['usd'] - $data['yesterday']['usd'];
 $data['diff']['eur'] = $data['today']['eur'] - $data['yesterday']['eur'];
 
+if($data['diff']['usd'] == 0 && $data['diff']['eur'] == 0) {
+    $data['diff']['usd'] = $data['tommorow']['usd'] - $data['today']['usd'];
+    $data['diff']['eur'] = $data['tommorow']['eur'] - $data['today']['eur'];
+}
+
 
 $output = [];
 
-$output[] = 'USD: ' . $data['today']['usd'] . '  ' . ($data['diff']['usd'] > 0 ? '+' : '-') . $data['diff']['usd'];
-$output[] = 'EUR: ' . $data['today']['eur'] . '  ' . ($data['diff']['eur'] > 0 ? '+' : '-') . $data['diff']['eur'];
+$output[] = 'USD: ' . $data['today']['usd'] . '  ' . ($data['diff']['usd'] > 0 ? '+' : '-') . abs($data['diff']['usd']);
+$output[] = 'EUR: ' . $data['today']['eur'] . '  ' . ($data['diff']['eur'] > 0 ? '+' : '-') . abs($data['diff']['eur']);
 
 $image = new Imagick();
 $draw = new ImagickDraw();
@@ -58,8 +66,11 @@ $draw->setFontSize( 14 );
 $image->annotateImage($draw, 10, 15, 0, $output[0]);
 $image->annotateImage($draw, 10, 35, 0, $output[1]);
 $draw->setFillColor( 'red' );
+#$image->annotateImage($draw, 10, 55, 0, $output[2]);
 
+/* Устанавливаем формат изображения */
 $image->setImageFormat('png');
 
+/* Выводим изображение с заголовками */
 header('Content-type: image/png');
-file_put_contents( __DIR__ . '/currency.png', $image);
+file_put_contents('/usr/share/nginx/cms.mobyman.org/currency.png', $image);
